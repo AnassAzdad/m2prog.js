2830750b147ae65d1b34ed2b0fff49425df554f2
bukayosaka11 <37286@ma-web.nl>
2023-11-20 14:39:36 +0100

    (Opdracht1B)

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ class App
 { 
     runApplication()
     {
-      console.log("hello world!");
-             //code gaat hier!
+        let EenVariable=("hello World!")
+        let eenVariableNaam = ("true");
+        let AppNaam = ("DemoApp");
+        let Versienummer = ("0.9");
+        let versiedatum = ("Oud");
+        let autheur=("Student");
+        let copyright=("Student BV");
+        let distributeur=("Student MBO");
+        let darkmode=("False");
+        
+        console.log("hello world!");
+        console.log("true");
+        console.log("DemoApp");
+        console.log("0.9");
+        console.log("Oud");
+        console.log("Student");
+        console.log("Student BV");
+        console.log("Student MBO");
+        console.log("False");
+       
+             
     }
 }
 
