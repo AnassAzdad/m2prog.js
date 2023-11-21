a2a501af0ffe2bc4f7782d00dfb4f32c93694f9f
bukayosaka11 <37286@ma-web.nl>
2023-11-21 12:51:19 +0100

    (1A)

---
+++ M1A/app.js
@@ -0,0 +1,30 @@
+class App
+{ 
+    runApplication()
+    {
+        let EenVariable=("hello World!")
+        let eenVariableNaam = ("true");
+        let AppNaam = ("DemoApp");
+        let Versienummer = ("1");
+        let versiedatum = ("Oud");
+        let autheur=("Student");
+        let copyright=("Student BV");
+        let distributeur=("Student MBO");
+        let darkmode=("False");
+        
+        console.log("hello world!");
+        console.log("true");
+        console.log("DemoApp");
+        console.log("1");
+        console.log("Oud");
+        console.log("Student");
+        console.log("Student BV");
+        console.log("Student MBO");
+        console.log("False");
+       
+             
+    }
+}
+
+let app = new App();
+app.runApplication();
