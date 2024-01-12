57d4c029a3a200dc85c9a29fcaa8528656eee56f
AnassAzdad <anassazdad3@gmail.com>
2024-01-12 10:09:08 +0100

    (07)

--- 07 classes/app.js
+++ 07 classes/app.js
@@ -10,18 +10,27 @@ class Greet
 {
     constructor()
     {
-        console.log("Greetings!");
     }
+  showGreeting()
+{
+    console.log("Greetings!");
+}
 }
 class Goodbye
 {
     constructor()
     {
-        console.log("Bye Bye");
     }
+   ShowBye()
+   {
+    console.log("Bye Bye");
+   }
 }
 
 let bye = new Goodbye();
 let greet = new Greet()
 let app = new App();
 app.runApplication();
+greet.showGreeting();
+bye.ShowBye();
+
